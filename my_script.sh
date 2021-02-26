e412b83311c97fd2e867b1cc07d64dc222271b98
Guit <ggojard@gmail.com>
2021-02-26 16:01:14 +0100

    remove cd doc root, useless

--- my_script.sh
+++ my_script.sh
@@ -1,8 +1,8 @@
-# cd $DOCUMENT_ROOT/
-
 npm install
 npm run build
 npm install express
 
-mkdir tmp/
-touch tmp/restart.txt
+mkdir $DOCUMENT_ROOT/tmp/
+touch $DOCUMENT_ROOT/tmp/restart.txt
+
+echo '-- End of post install script --'
