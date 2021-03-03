884711b315d842c5ed7b44e2fed3beb4b283c0f9
Guit <ggojard@gmail.com>
2021-03-03 18:35:24 +0100

    another try

--- my_script.sh
+++ my_script.sh
@@ -1,13 +1,14 @@
+echo '-- Start post install script'
 echo '-- npm commands'
-echo ''
 npm install
 npm run build
 npm install express
 
 echo '-- Restart hosting runtime '
-echo ''
 mkdir tmp/
 touch tmp/restart.txt
+# mkdir $HOME/$DOCUMENT_ROOT/tmp/
+# touch $HOME/$DOCUMENT_ROOT/tmp/restart.txt
+
 
 echo '-- End of post install script'
-echo ''
