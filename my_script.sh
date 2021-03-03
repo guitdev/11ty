dee829201b77af7566ccf1b0d3d1eabefcb940dc
Guit <ggojard@gmail.com>
2021-03-03 14:59:01 +0100

    modify version deploy

--- my_script.sh
+++ my_script.sh
@@ -1,8 +1,13 @@
+echo '-- npm commands'
+echo ''
 npm install
 npm run build
 npm install express
 
+echo '-- Restart hosting runtime '
+echo ''
 mkdir tmp/
 touch tmp/restart.txt
 
-echo '-- End of post install script --'
+echo '-- End of post install script'
+echo ''
